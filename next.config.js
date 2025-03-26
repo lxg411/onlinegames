25b9f66d1cf064dfcc9e1b0557ba02e2443d4a23
lxg411 <33448727@qq.com>
2025-03-26 15:11:28 +0800

    修复配置错误

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     minimumCacheTTL: 60,
-    formats: ['image/webp', 'image/jpeg', 'image/png'],
+    formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
